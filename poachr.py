04995c9b0f04c610cd3828c7786d326e27c1b43d
Bertrand Fan <bertrand@fan.net>
2011-11-18 03:35:47 +0000

    Initial import

---
+++ poachr.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import urllib2
+from BeautifulSoup import BeautifulSoup
+import re
+import os, os.path
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+
+response = urllib2.urlopen('http://www.flickr.com/about/')
+html = response.read()
+
+soup = BeautifulSoup(html)
+
+buddyicons = soup.findAll('img', { "class" : "personmenu-trigger"})
+
+names = []
+nsids = []
+
+for buddyicon in buddyicons:
+	names.append(buddyicon["alt"])
+	x = re.search('buddyicons\/(.*).jpg', buddyicon["src"])
+	nsids.append(x.group(1))
+	
+name_file = open(script_path + '/' + 'names.txt', 'w')
+
+for name in names:
+  name_file.write("%s\n" % name)
+
+name_file.close()
+
+nsid_file = open(script_path + '/' + 'nsids.txt', 'w')
+
+for nsid in nsids:
+  nsid_file.write("%s\n" % nsid)
+
+nsid_file.close()
