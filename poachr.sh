5d933da3cf38a97c3edb5e51477e77ae4a404703
Bertrand Fan <bertrand@fan.net>
2011-11-18 04:09:39 +0000

    Don't commit when there are no changes

--- poachr.sh
+++ poachr.sh
@@ -3,7 +3,10 @@ TODAY=$(date)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 ./poachr.py
-git add names.txt
-git add nsids.txt
-git commit -m "Updating $TODAY"
-git push origin master
+git diff --exit-code
+if [ "$?" -ne "0" ]; then
+  git add names.txt
+  git add nsids.txt
+  git commit -m "Updating $TODAY"
+  git push origin master
+fi
